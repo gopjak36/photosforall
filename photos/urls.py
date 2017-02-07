54683942decf7cde20354cbf868431dffb15c2a7
gopjak36 <gop.jak.36@gmail.com>
2017-02-07 22:10:43 +0200

    Added details for photos

--- photos/urls.py
+++ photos/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.photos_list, name='photos_list')
+    url(r'^$', views.photos_list, name='photos_list'),
+    url(r'^photo/(?P<pk>[0-9]+)/$', views.photo_detail, name='photo_detail'),
 ]
--- photos/views.py
+++ photos/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Photo
 
 def photos_list(request):
     photos = Photo.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'photos/photos_list.html', {'photos':photos})
+
+def photo_detail(request, pk):
+    photo = get_object_or_404(Photo, pk=pk)
+    return render(request, 'photos/photo_detail.html', {'photo': photo})
