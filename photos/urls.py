60c540b91b5a878af4e0eafc5a889a0df21ab8c5
gopjak36 <gop.jak.36@gmail.com>
2017-02-09 22:22:29 +0200

    Added form to add new photo

--- photos/urls.py
+++ photos/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     url(r'^$', views.photos_list, name='photos_list'),
     url(r'^photo/(?P<pk>[0-9]+)/$', views.photo_detail, name='photo_detail'),
+    url(r'^photo/new/$', views.photo_new, name='photo_new'),
 ]
--- photos/views.py
+++ photos/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Photo
+from .forms import PhotoForm
 
 def photos_list(request):
     photos = Photo.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
@@ -9,3 +10,15 @@ def photos_list(request):
 def photo_detail(request, pk):
     photo = get_object_or_404(Photo, pk=pk)
     return render(request, 'photos/photo_detail.html', {'photo': photo})
+
+def photo_new(request):
+    if request.method == "POST":
+        form = PhotoForm(request.POST)
+        if form.is_valid():
+            photo = form.save(commit = False)
+            photo.published_date = timezone.now()
+            photo.save()
+            return redirect ('photo_detail', pk=photo.pk)
+    else:
+        form = PhotoForm()
+    return render(request, 'photos/photo_edit.html', {'form': form})
